baf8bb4043ca5c43b08cf28b3f7b8f55be7b0db8
calmpsycho333 <wrathking75@yahoo.com>
2020-12-16 12:34:20 -0500

    commit tree adedd477b147d5fb5cfc4e9e2103e67707670522

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry' 
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarves.each.with_index(1) do |name, index|
+    puts "#{index}. #{name}"
+  end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map! do | name |
+    #binding.pry
+     "#{name.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any?{|x| x.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|  cheese_types.include?(cheese)
 end
+end 
